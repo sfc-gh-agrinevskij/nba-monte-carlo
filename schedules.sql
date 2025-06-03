c854adc58ce421723c84f5a7b4ef156d6caae370
sfc-gh-agrinevskij <anton.grinevskij@snowflake.com>
2025-06-03 21:59:02 +0000

    update

--- schedules.sql
+++ schedules.sql
@@ -2,3 +2,6 @@ create or replace task NBA_MONTE_CARLO.PIPELINE.HOURLY_SIMULATION
 	warehouse=NBA_MONTE_CARLO_WH
 	schedule='USING CRON 0 */4 * * * America/Los_Angeles'
 	as EXECUTE dbt project NBA_SIMULATION args='build --target dev';
+
+
+alter task NBA_MONTE_CARLO.PIPELINE.HOURLY_SIMULATION resume;
